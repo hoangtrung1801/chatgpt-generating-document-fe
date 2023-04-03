ffdd00876ea368a0e4a15903d1da6b6d54d0d266
Hoang Trung <hoangtrung1801.2003@gmail.com>
2023-04-03 15:52:05 +0700

    fix: authen when requesting api

--- src/lib/hooks/useGetBriefs.ts
+++ src/lib/hooks/useGetBriefs.ts
@@ -1,10 +1,11 @@
 import { BE_URL } from "lib/config";
+import { fetchWithCredentials } from "lib/fetcher";
 import useSwr, { SWRResponse } from "swr";
 
 const API_URL = `${BE_URL}/chatgpt/briefs`;
 
 const useBriefs = () => {
-    const { data, error }: SWRResponse = useSwr(API_URL);
+    const { data, error }: SWRResponse = useSwr(API_URL, fetchWithCredentials);
 
     return {
         briefs: data !== undefined ? data.data : [],
